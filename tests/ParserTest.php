bf3017b784d81ce736ac8f89425d896953e2d765
Larry Garfield <larry@garfieldtech.com>
2023-04-07 17:19:24 -0500

    We don't care about argc.

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -15,7 +15,7 @@ class ParserTest extends TestCase
      * @test
      * @dataProvider exampleSuccessArgs()
      */
-    public function success(int $argc, array $argv, string $class, object $expected): void
+    public function success(array $argv, string $class, object $expected): void
     {
         $parser = new Parser();
         $result = $parser->parse($argv, to: $class);
@@ -27,7 +27,7 @@ class ParserTest extends TestCase
      * @test
      * @dataProvider exampleErrorArgs()
      */
-    public function errors(int $argc, array $argv, string $class, string $expectedException): void
+    public function errors(array $argv, string $class, string $expectedException): void
     {
         $this->expectException($expectedException);
         $parser = new Parser();
@@ -37,31 +37,26 @@ class ParserTest extends TestCase
     public function exampleSuccessArgs(): iterable
     {
         yield 'basic long-name parameter' => [
-            'argc' => 1,
             'argv' => ['script.php', '--about=A'],
             'class' => Basic::class,
             'expected' => new Basic('A'),
         ];
         yield 'basic short-name parameter' => [
-            'argc' => 1,
             'argv' => ['script.php', '-a=A'],
             'class' => Basic::class,
             'expected' => new Basic('A'),
         ];
         yield 'multi-value long parameter' => [
-            'argc' => 3,
             'argv' => ['script.php', '--file=A', '--file=B'],
             'class' => Multivalue::class,
             'expected' => new Multivalue(['A', 'B']),
         ];
         yield 'typed arguments with only one array value' => [
-            'argc' => 5,
             'argv' => ['script.php', '--int=5', '--string=world', '--float=2.7', '--array=val'],
             'class' => Typed::class,
             'expected' => new Typed(5, 'world', 2.7, ['val']),
         ];
         yield 'typed arguments with multiple array values' => [
-            'argc' => 5,
             'argv' => ['script.php', '--int=5', '--string=world', '--float=2.7', '--array=beep', '--array=boop'],
             'class' => Typed::class,
             'expected' => new Typed(5, 'world', 2.7, ['beep', 'boop']),
@@ -71,7 +66,6 @@ class ParserTest extends TestCase
     public function exampleErrorArgs(): iterable
     {
         yield [
-            'argc' => 1,
             'argv' => ['script.php', '--about=A', '--C'],
             'class' => Basic::class,
             'expectedException' => \InvalidArgumentException::class,
